d9c76b0d6d369095bb29d580ac30839f509bc0b6
Shaheem PP <shanofficial2000@gmail.com>
2024-09-28 20:06:03 -0400

    28 09 2024

---
+++ Dattebayo/Dattebayo/Model/CharactersModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersModel.swift
+//  Dattebayo
+//
+//  Created by Shaheem PP on 12/09/2024.
+//
+
+import Foundation
---
+++ Dattebayo/Dattebayo/ViewModel/CharactersViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersViewModel.swift
+//  Dattebayo
+//
+//  Created by Shaheem PP on 12/09/2024.
+//
+
+import Foundation
